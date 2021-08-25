3df1526753313bbfce2100fccd99e1b99f10fbe7
Toksisitee <loading.buzzz@gmail.com>
2021-08-25 15:36:08 +0300

    Common.h

---
+++ Common.h
@@ -0,0 +1,52 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <QtCore/QtGlobal>
+#include <QDataStream>
+#include <QIODevice>
+#include <QFile>
+#include <QTableWidget>
+
+template <typename T>
+struct Bank {
+    uint32_t Entries;
+    QVector<QPair<uint32_t, T>> Entry;
+};
+
+
+class CBank
+{
+public:
+    virtual         ~CBank() {};
+    virtual bool    Load(const QString& filename) = 0;
+    virtual void    Create(const QString& filename) = 0;
+    virtual void    Export(uint32_t index) = 0;
+    virtual void    Play(uint32_t index) = 0;
+    virtual void    FillTable(QTableWidget* table) = 0;
+    virtual int32_t GetEntries() const = 0;
+protected:
+    virtual void    Clear() = 0;
+    char*           m_pBuffer;
+    uint32_t        m_nBufferLength;
+};
+
+// http://soundfile.sapp.org/doc/WaveFormat/
+struct WAVE
+{
+    char		RIFF[4];        // RIFF Header      Magic header
+    uint32_t    ChunkSize;      // RIFF Chunk Size
+    char		WAVE[4];        // WAVE Header
+
+    char		fmt[4];         // FMT header
+    uint32_t	Subchunk1Size;  // Size of the fmt chunk
+    uint16_t	AudioFormat;    // Audio format 1=PCM,6=mulaw,7=alaw, 257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
+    uint16_t	NumChannels;    // Number of channels 1=Mono 2=Sterio
+    uint32_t	SampleRate;     // Sampling Frequency in Hz
+    uint32_t	ByteRate;       // bytes per second
+    uint16_t	BlockAlign;     // 2=16-bit mono, 4=16-bit stereo
+    uint16_t	BitsPerSample;  // Number of bits per sample
+    char		Subchunk2ID[4]; // "data"  string
+    uint32_t	Subchunk2Size;  // Sampled data length
+};
+
+#endif // COMMON_H
