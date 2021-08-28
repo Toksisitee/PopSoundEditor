c7372fcdb03186f4d1eef320beeb06a13d5bd5ab
Toksisitee <loading.buzzz@gmail.com>
2021-08-28 13:13:36 +0300

    setApplicationVersion use EDITOR_VERSION macro

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
     a.setOrganizationDomain("https://github.com/Toksisitee");
     a.setOrganizationName("Toksisitee");
     a.setApplicationName(EDITOR_NAME);
-    a.setApplicationVersion("v1.0.0");
+    a.setApplicationVersion(EDITOR_VERSION);
     MainWindow w;
     w.show();
     return a.exec();
